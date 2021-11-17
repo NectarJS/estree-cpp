f4d0a634c34bb3c03a517edc2cf4beb4abb8e739
Saiv46 <saiv46@yandex.ru>
2021-11-17 21:20:06 +0800

    Update cli.js

--- cli.js
+++ cli.js
@@ -1,21 +1,36 @@
 #!/usr/bin/env node
 const estreecpp = require('./index')
-const { createReadStream } = require('fs')
-const [,, inputFile] = process.argv
+const { once } = require('events')
+const { createReadStream, createWriteStream } = require('fs')
+const [,, inputFile, outputFile] = process.argv
 
 if (!inputFile) {
-	console.log('Usage: estree-cpp example.js\n- stdin as input: estree-cpp -\n- file as output: estree-cpp example.js > output.cpp')
+	console.log('Usage: estree-cpp <input> [output]\n  Takes JS script from file or stdin and outputs C++ code to file or stdout')
 	process.exit(0)
 }
 
-const chunks = []
-const stream = inputFile === '-' ? process.stdin : createReadStream(inputFile)
-stream.on('data', chunk => chunks.push(Buffer.from(chunk)))
-stream.on('error', err => {
-	console.error(err)
-	process.exitCode = 1
-})
-stream.on('end', () => {
-	const str = Buffer.concat(chunks).toString('utf8')
-	console.log(estreecpp(str))
-})
+async function readFullStream (stream) {
+	const chunks = []
+	for await (const chunk of stream) {
+		chunks.push(Buffer.from(chunk))
+	}
+	return Buffer.concat(chunks)
+}
+
+async function outputFullStream (stream, data) {
+	for (let i = 0; i < data.length;) {
+		const flushed = stream.write(data.slice(i, i += stream.writableHighWaterMark))
+		if (!flushed) {
+			await once(stream, 'drain')
+		}
+	}
+	stream.end()
+}
+
+async function main () {
+	const inputStream = inputFile === '-' ? process.stdin : createReadStream(inputFile)
+	const inputBuffer = await readFullStream(inputStream)
+	const outputStream = !outputFile || outputFile === '-' ? process.stdout : createWriteStream(outputFile)
+	await outputFullStream(outputStream, estreecpp(inputBuffer.toString('utf-8')))
+}
+main()
